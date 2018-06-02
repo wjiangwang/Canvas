5c443bd7f9d7ab918d4052746408d9869dcb4861
wwj <664027523@qq.com>
2018-06-02 16:41:41 +0800

    画笔粗细，下载功能，清屏。（一些bug）

--- canvas/main.js
+++ canvas/main.js
@@ -1,98 +1,102 @@
 var yyy = document.getElementById('xxx')
 var ctx = yyy.getContext('2d');
 
+
+
 /****设置屏幕尺寸*****/
 autoWindowSize()
+
 /*监听鼠标*/
 listenUser(yyy)
+
 /*橡皮擦*/
 var eraserEnable = false
 eraser.onclick = function () {
     eraserEnable = true
-    eraser.classList.add ('active')
+    eraser.classList.add('active')
     pen.classList.remove('active')
 
 }
 pen.onclick = function () {
     eraserEnable = false
     pen.classList.add('active')
-    eraser.classList.remove ('active')
+    eraser.classList.remove('active')
 }
+
+
 /*颜色选择*/
 //var colors={red:red,black:black,blue:blue,green:green}
-black.onclick=function( ){
+black.onclick = function () {
     ctx.strokeStyle = 'black'
     black.classList.add('active')
-    yellow.classList.remove ('active')
-    blue.classList.remove ('active')
-    red.classList.remove ('active')
-    green.classList.remove ('active')
+    yellow.classList.remove('active')
+    blue.classList.remove('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
 }
-yellow.onclick=function( ){
+yellow.onclick = function () {
     ctx.strokeStyle = 'yellow'
     yellow.classList.add('active')
-    black.classList.remove ('active')
-    blue.classList.remove ('active')
-    green.classList.remove ('active')
-    red.classList.remove ('active')
+    black.classList.remove('active')
+    blue.classList.remove('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
 }
-blue.onclick=function( ){
+blue.onclick = function () {
     ctx.strokeStyle = 'blue'
     blue.classList.add('active')
-    red.classList.remove ('active')
-    yellow.classList.remove ('active')
-    black.classList.remove ('active')
-    green.classList.remove ('active')
+    red.classList.remove('active')
+    yellow.classList.remove('active')
+    black.classList.remove('active')
+    green.classList.remove('active')
 }
-red.onclick=function( ){
+red.onclick = function () {
     ctx.strokeStyle = 'red'
     red.classList.add('active')
-    blue.classList.remove ('active')
-    yellow.classList.remove ('active')
-    black.classList.remove ('active')
-    green.classList.remove ('active')
+    blue.classList.remove('active')
+    yellow.classList.remove('active')
+    black.classList.remove('active')
+    green.classList.remove('active')
 }
-green.onclick=function( ){
+green.onclick = function () {
     ctx.strokeStyle = 'green'
     green.classList.add('active')
-    blue.classList.remove ('active')
-    yellow.classList.remove ('active')
-    black.classList.remove ('active')
-    red.classList.remove ('active')
+    blue.classList.remove('active')
+    yellow.classList.remove('active')
+    black.classList.remove('active')
+    red.classList.remove('active')
 
 }
 
 
+/*画笔粗细*/
+thin.onclick = function () {
+    lineWidth = 5
+}
+thick.onclick = function () {
+    lineWidth = 10
+}
 
 
+/*清屏*/
+clear.onclick = function () {
+    ctx.clearRect(0, 0, yyy.width, yyy.height);
+
+}
+
+/*下载*/
+save.onclick = function () {
+    var imagedata = yyy.toDataURL('png')
+    a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = imagedata
+    a.target = '_blank'
+    a.download = '我的画布'
+    a.click()
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*工具函数*/
 function listenUser(canvas) {
     var lastPoint = { x: undefined, y: undefined }
     var useing = false
@@ -102,7 +106,7 @@ function listenUser(canvas) {
         yyy.ontouchstart = function (aaa) {
             var x = aaa.touches['0'].clientX
             var y = aaa.touches['0'].clientY
-            console.log(x,y)
+            console.log(x, y)
             useing = true
             if (eraserEnable) {
                 ctx.clearRect(x - 5, y - 5, 10, 10)
@@ -169,18 +173,14 @@ function listenUser(canvas) {
 
     }
 }
-
-
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath()
     ctx.moveTo(x1, y1)//上一个点
-    ctx.lineWidth = 4
+    ctx.lineWidth = lineWidth
     ctx.lineTo(x2, y2)//新的点
     ctx.stroke()
     ctx.closePath()
 }
-
-
 function autoWindowSize() {
     function getWH() {//获取宽高
         xxx.width = document.documentElement.clientWidth
@@ -190,7 +190,11 @@ function autoWindowSize() {
     window.onresize = function () {
         getWH()
     }
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, yyy.width, yyy.height);
 }
+
+
 
 
 
